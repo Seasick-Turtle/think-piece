ff255622acc512b0c0122a5980962b2098e8f0af
Christopher Martin <martincdev@gmail.com>
2019-03-28 19:51:23 -0500

    added firebase/configuration to app"

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyDwb0lAiCJfXU14nuD-CBZX4yGdgajpN2Y",
+  authDomain: "think-piece-94b7a.firebaseapp.com",
+  databaseURL: "https://think-piece-94b7a.firebaseio.com",
+  projectId: "think-piece-94b7a",
+  storageBucket: "think-piece-94b7a.appspot.com",
+  messagingSenderId: "244291724793"
+};
+firebase.initializeApp(config);
+
+export const firestore = firebase.firestore();
+
+window.firebase = firebase;
+
+export default firebase;
