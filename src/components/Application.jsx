fbda76c272f2319aa4000f8e0a2aa39bd6a1cdc1
Christopher Martin <martincdev@gmail.com>
2019-03-30 17:25:17 -0500

    Refactor adding and removing posts to avoid manually adding state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -10,34 +10,18 @@ class Application extends Component {
     posts: []
   };
 
+  unsubscribe = null;
+
   componentDidMount= async () => {
-    const snapshot = await firestore.collection('posts').get();
-
-    const posts = snapshot.docs.map(collectIdsAndDocs);
-
-    this.setState({ posts });
+    this.unsubscribe = firestore.collection('posts').onSnapshot(snapshot => {
+      const posts = snapshot.docs.map(collectIdsAndDocs);
+      this.setState({ posts });
+    });
   };
 
-  handleCreate = async post => {
-    const { posts } = this.state;
-
-    const docRef = await firestore.collection('posts').add(post);
-    const doc = await docRef.get();
-
-    const newPost = collectIdsAndDocs(doc);
-
-    this.setState({ posts: [newPost, ...posts] });
-  };
-
-  handleRemove = async id => {
-    const allPosts = this.state.posts;
-
-    await firestore.doc(`posts/${id}`).delete();
-
-    const posts = allPosts.filter(post => post.id !== id);
-
-    this.setState({ posts });
-  };
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
 
   render() {
     const { posts } = this.state;
@@ -47,8 +31,7 @@ class Application extends Component {
         <h1>Think Piece</h1>
         <Posts
           posts={posts}
-          onCreate={this.handleCreate}
-          onRemove={this.handleRemove} />
+        />
       </main>
     );
   }
